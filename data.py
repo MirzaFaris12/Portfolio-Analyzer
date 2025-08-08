b5357262ed03a33419e2bea920a5e156ed15f9c6
Mirza Faris Bin Mohd Affandi <156393988+MirzaFaris12@users.noreply.github.com>
2025-08-07 21:55:34 -0400

    Update data.py

--- data.py
+++ data.py
@@ -1,1 +1,20 @@
+import yfinance as yf
+import pandas as pd
+
+def fetch_market_data(tickers):
+    prices = {}
+    sectors = {}
+    for ticker in tickers:
+        try:
+            stock = yf.Ticker(ticker)
+            prices[ticker] = stock.info['regularMarketPrice']
+            sectors[ticker] = stock.info.get("sector", "Unknown")
+        except:
+            prices[ticker] = 0
+            sectors[ticker] = "Unknown"
+    return pd.DataFrame({
+        "Ticker": tickers,
+        "Price": [prices[t] for t in tickers],
+        "Sector": [sectors[t] for t in tickers]
+    })
 
